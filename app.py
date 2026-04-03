f5a39b0c1fc1382e2e9e6de2363839dadb3de976
G VIJAY S PAWAR <vijaypawarg9899@gmail.com>
2026-04-03 19:41:53 +0530

    changes made now

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
 def sum(a,b,c):
     return a + b + c
 
-<<<<<<< HEAD
 def sub(a,b,d):
     return a - b - d
-=======
-def sub(a,b,e):
-    return a - b - e
->>>>>>> efdf346e671f728ce0e53a23698550c627a74ce4
 
 def dev(a,b):
     return a / b
